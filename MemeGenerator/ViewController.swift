b73aefbc5e57848472171f4355cf3ad30cd85136
Helder Sérvio <heldersrvio@gmail.com>
2021-03-14 17:40:29 -0300

    Fixed layout and display of text

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var selectImageButton: UIButton!
@@ -32,22 +32,31 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let pickedImage = pickedImage {
             let renderer = UIGraphicsImageRenderer(size: pickedImage.size)
             let image = renderer.image { ctx in
-                //pickedImage.draw(in: CGRect(x: 0, y: 0, width: pickedImage.size.width, height: pickedImage.size.height))
+                pickedImage.draw(in: CGRect(x: 0, y: 0, width: pickedImage.size.width, height: pickedImage.size.height))
                 let paragraphStyle = NSMutableParagraphStyle()
                 paragraphStyle.alignment = .center
-                let attrs: [NSAttributedString.Key: Any] = [
-                    .font: UIFont.systemFont(ofSize: 16),
-                    //.paragraphStyle: paragraphStyle,
-                    .foregroundColor: UIColor.blue,
-                    .backgroundColor: UIColor.yellow
+                let mainAttrs: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.systemFont(ofSize: 400, weight: .bold),
+                    .paragraphStyle: paragraphStyle,
+                    .foregroundColor: UIColor.white,
                 ]
-                if let topText = topText {
-                    let attributedString1 = NSAttributedString(string: topText, attributes: attrs)
+                let outlineAttrs: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.systemFont(ofSize: 400, weight: .bold),
+                    .paragraphStyle: paragraphStyle,
+                    .strokeWidth: 4,
+                    .strokeColor: UIColor.black,
+                ]
+                if let topText = topText?.uppercased() {
+                    let outlineAttributedString1 = NSAttributedString(string: topText, attributes: outlineAttrs)
+                    outlineAttributedString1.draw(in: CGRect(x: 0, y: 0, width: pickedImage.size.width, height: pickedImage.size.height))
+                    let attributedString1 = NSAttributedString(string: topText, attributes: mainAttrs)
                     attributedString1.draw(in: CGRect(x: 0, y: 0, width: pickedImage.size.width, height: pickedImage.size.height))
                 }
-                if let bottomText = topText {
-                    let attributedString2 = NSAttributedString(string: bottomText, attributes: attrs)
-                    attributedString2.draw(with: CGRect(x: 0, y: pickedImage.size.height - 48, width: 448, height: 448), options: .usesLineFragmentOrigin, context: nil)
+                if let bottomText = bottomText?.uppercased() {
+                    let outlineAttributedString2 = NSAttributedString(string: bottomText, attributes: outlineAttrs)
+                    outlineAttributedString2.draw(in: CGRect(x: 0, y: pickedImage.size.height - 530, width: pickedImage.size.width, height: pickedImage.size.height))
+                    let attributedString2 = NSAttributedString(string: bottomText, attributes: mainAttrs)
+                    attributedString2.draw(in: CGRect(x: 0, y: pickedImage.size.height - 530, width: pickedImage.size.width, height: pickedImage.size.height))
                 }
             }
             imageView.image = image
@@ -57,30 +66,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func presentTopTextAlert() {
         let ac = UIAlertController(title: "Add a line to the top of the image", message: nil, preferredStyle: .alert)
         ac.addTextField()
-        ac.addAction(UIAlertAction(title: "No, thanks", style: .cancel))
-        ac.addAction(UIAlertAction(title: "Add", style: .default, handler: { (_action) in
+        ac.addAction(UIAlertAction(title: "No, thanks", style: .cancel, handler: { (_action) in
+            self.topText = nil
+            self.dismiss(animated: true) {
+                self.presentBottomTextAlert()
+            }
+        }))
+        let addTextAction = UIAlertAction(title: "Add", style: .default, handler: { (_action) in
             if let text = ac.textFields?[0].text {
                 self.topText = text
             }
             self.dismiss(animated: true) {
                 self.presentBottomTextAlert()
             }
-        }))
+        })
+        ac.addAction(addTextAction)
         present(ac, animated: true)
     }
     
     func presentBottomTextAlert() {
         let ac = UIAlertController(title: "Add a line to the bottom of the image", message: nil, preferredStyle: .alert)
         ac.addTextField()
-        ac.addAction(UIAlertAction(title: "No, thanks", style: .cancel))
-        ac.addAction(UIAlertAction(title: "Add", style: .default, handler: { (_action) in
+        ac.addAction(UIAlertAction(title: "No, thanks", style: .cancel, handler: { (_action) in
+            self.bottomText = nil
+            self.dismiss(animated: true) {
+                self.drawMeme()
+            }
+        }))
+        let addTextAction = UIAlertAction(title: "Add", style: .default, handler: { (_action) in
             if let text = ac.textFields?[0].text {
                 self.bottomText = text
             }
             self.dismiss(animated: true) {
                 self.drawMeme()
             }
-        }))
+        })
+        ac.addAction(addTextAction)
         present(ac, animated: true)
     }
     
@@ -94,6 +115,5 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             dismiss(animated: true)
         }
     }
-    
 }
 
